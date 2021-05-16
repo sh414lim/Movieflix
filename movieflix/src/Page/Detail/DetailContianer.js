1cf633057a40ff2efa43a926d586a925b2bf933a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-16 19:52:51 +0900

    .

--- movieflix/src/Page/Detail/DetailContianer.js
+++ movieflix/src/Page/Detail/DetailContianer.js
@@ -1,39 +1,65 @@
 import React from "react";
+import { Moviesapi, tvApi } from "../../api";
 import DetailPresenter  from "./DetailPresenter";
 
 
 export default class extends React.Component{
-    state={
+    constructor(props){
+        super(props)
+        //props는 {pathname} 이 아니다,pathname 은 존재하지 않는다 super(props) 이건 생성자 클래스 이다
+        const {location:{pathname}}=props;
+        this.state={
 
-        //shoow,movie 를 찾을때 id를 가지고 가는데
-        //그 작업들이 끝나면 그걸로 검색하고 결과값(result)을 보여준디.
-        result:null,
+            //shoow,movie 를 찾을때 id를 가지고 가는데
+            //그 작업들이 끝나면 그걸로 검색하고 결과값(result)을 보여준디.
+            result:null,
+            error:null,
+            loading:true,
+        isMovie:pathname.includes("/movie/")
 
-
-        error:null,
-        loading:true
-    };
-
+        };
+    
+    }
+   
     async componentDidMount(){
-        const {
-            match:{
-                params:{id}
-            },
-            history:{push},
-            location:{pathname}
-            }=this.props;
-
-            this.ismovie=pathname.includes("/movie/");
-            const parsedId = parseInt(id);
-            if(isNaN(parsedId)){
-             return    push("/");
-            }
-            console.log(this.ismovie);
+      const {
+          match:{
+            params:{id}
+    },
+    history:{push},
+    }
+        =this.props;
+        const {isMovie} = this.state;
+        const parseId = parseInt(id);
+        if(isNaN(parseId)){
+         return  push("/");
+            //return 할 때 함수는 종료한다.
         }
+        let result=null
+        try{
+            if(isMovie){
+           ({data:result}= await Moviesapi.movieDetail(parseId));
+
+            }else{
+            ({data:result}= await tvApi.showDetail(parseId));
+            }
+            console.log(result);
+        } catch{
+            this.setState({
+                error:"Can't Find.....!"
+            })
+        }finally{
+            this.setState({
+                loading:false,result
+            })
+        }
+    }
+
+  
 
     render(){
-        console.log(this.props)
         const {result,error,loading} = this.state
+        console.log(result);
         return(
             <DetailPresenter
             result={result}
